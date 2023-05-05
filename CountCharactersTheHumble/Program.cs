eb68fa40f35acabcc8f9b08eb75ba51a25bd8a0a
VictorVariety <victorv@getacademy.no>
2023-05-05 21:06:54 +0200

    Initial commit

---
+++ CountCharactersTheHumble/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace CountCharactersTheHumble
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ReadTextAndShowCharacterCounts(255);
+        }
+
+        private static void ReadTextAndShowCharacterCounts(int range)
+        {
+            string text = "whatever";
+            while (!string.IsNullOrWhiteSpace(text))
+            {
+                Console.WriteLine("Type anything");
+                text = Console.ReadLine().ToLower();
+
+                var counts = CharacterCounts(range, text);
+                PrintCounts(range, counts, text);
+            }
+        }
+
+        private static void PrintCounts(int range, int[] counts, string text)
+        {
+            for (int i = 0; i < range; i++)
+            {
+                if (counts[i] == 0) continue;
+                var character = (char)i;
+                //(char) tvinger det tilbake til en tegn.
+                var percent = (counts[i] * 100) / text.Length;
+
+                Console.WriteLine("\"" + character + "\" - " + counts[i] + " Which is " + percent + "% of total characters");
+            }
+        }
+
+        private static int[] CharacterCounts(int range, string text)
+        {
+            var counts = new int[range];
+            foreach (var character in text)
+            //En string "er" en array av tegnene stringen
+            {
+                counts[(int)character]++;
+                //(int)character tvinger character å bli tolket som en int, ASCII
+            }
+            return counts;
+        }
+    }
+}
